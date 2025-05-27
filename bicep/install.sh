e5a7e2d5b7540f5fc3e9d5ba8d856a85f5a331c3
Michael Weiser <michael.weiser@eviden.com>
2025-05-27 11:54:57 +0200

    Add EF Portal parameter patch

--- bicep/install.sh
+++ bicep/install.sh
@@ -327,6 +327,10 @@ case "$CLUSTER_TYPE" in
 		wget -O local-scheduler.patch https://raw.githubusercontent.com/mw-a/cyclecloud-local-scheduler/main/templates/openpbs.patch
 		patch templates/openpbs.txt < local-scheduler.patch
 
+		# add EF Portal/DCV support
+		wget -O ef.patch https://raw.githubusercontent.com/mw-a/cyclecloud-ef/main/templates/openpbs.patch
+		patch templates/openpbs.txt < ef.patch
+
 		cyclecloud import_template -c OpenPBS -f templates/openpbs.txt ${CLUSTER_PROJ_NAME}_template_${CLUSTER_PROJ_VERSION} --force
 		popd
 		(python3 create_cc_param.py pbs) > cluster_params.json
